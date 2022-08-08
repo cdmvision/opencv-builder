881feb529b3b13d8fd60ce6167e16f886ce85fb8
Guner Acet <kurama632@gmail.com>
2022-08-08 19:10:16 +0300

    opencv modifier and config update

--- modify_opencv.py
+++ modify_opencv.py
@@ -8,7 +8,7 @@ class VARS:
     opencv_cmake_module_path = "opencv/cmake/OpenCVModule.cmake"
     target_line_sign = "  # For dynamic link numbering conventions"
     actual_line = "  if(NOT ANDROID)"
-    updated_line = "  if(NOT (ANDROID OR UNIX OR WIN32) )"
+    updated_line = "  if(NOT ANDROID AND NOT UNIX AND NOT WINRT)"
 
 
 
